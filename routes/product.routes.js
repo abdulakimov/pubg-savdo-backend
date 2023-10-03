086ad2ab979d754efb91a20f183cafaf56b55855
Xurshidbek <xurshidbekabdulakimov@gmail.com>
2023-10-04 03:15:44 +0500

    Finishing authentication and authorization

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,8 +4,8 @@ import { adminAuth, auth } from "../auth/auth.js";
 const router = Router();
 
 router.get("/product", productController.getProduct);
-router.post("/product", auth, productController.createProduct);
-router.put("/product/:id", auth, productController.updateProduct);
-router.delete("/product/:id", auth, productController.deleteProduct);
+router.post("/product", productController.createProduct);
+router.put("/product/:id", productController.updateProduct);
+router.delete("/product/:id", productController.deleteProduct);
 
 export default router;
